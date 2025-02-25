45794f778cfe68cdfb04f4d7d4f7ff26477d696b
KPousse <kilian.psse@gmail.com>
2025-02-25 16:33:23 +0100

    add header box
    +app-header-box (conponent)

---
+++ src/app/components/header-box/header-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-box.component.html',
+  styleUrl: './header-box.component.css'
+})
+export class HeaderBoxComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HeaderBoxComponent } from './app/components/header-box/header-box.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HeaderBoxComponent, appConfig)
   .catch((err) => console.error(err));
