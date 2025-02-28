9d0a9affdb6e8e9b0530898b7adbfe979f76d8b9
KPousse <kilian.psse@gmail.com>
2025-02-28 11:15:33 +0100

    add styles

---
+++ src/app/components/card-box/card-box.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-box',
+  standalone: true,
+  templateUrl: './card-box.component.html',
+  styleUrls: ['./card-box.component.css']
+})
+export class CardBoxComponent { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { HeaderBoxComponent } from './app/components/header-box/header-box.component';
+import { CardBoxComponent } from './app/components/card-box/card-box.component';
 
 bootstrapApplication(HeaderBoxComponent, appConfig)
   .catch((err) => console.error(err));
+
+bootstrapApplication(CardBoxComponent, appConfig)
+    .catch((err) => console.error(err));
