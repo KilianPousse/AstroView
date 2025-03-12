f03fac51763929000198a9dc8810e8955dede330
KPousse <kilian.psse@gmail.com>
2025-03-12 17:02:14 +0100

    test avec apod

--- front/src/app/components/apod-box/apod-box.component.ts
+++ front/src/app/components/apod-box/apod-box.component.ts
@@ -1,39 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'
 import { ApiApodService } from '../../services/apod/api-apod.service';
 
 
 @Component({
   selector: 'app-apod-box',
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './apod-box.component.html',
   styleUrls: ['./apod-box.component.css']
 })
-export class ApodBoxComponent {
+
+
+export class ApodBoxComponent implements OnInit {
   apod: any = null;
 
   constructor(private api: ApiApodService) { }
 
   ngOnInit(): void {
-    this.api.getAPOD().subscribe({
-      next: (res) => {
+    this.api.getAPOD().subscribe( res=> {
         console.log('Récupération de l\'APOD');
         this.apod = res;
-      },
-      error: (err) => {
-        console.error('fetchAPOD a échoué: ', err);
-      }
     });
   }
 
-  // Définir les méthodes onImageLoad et onImageError
-  onImageLoad() {
-    console.log("Image chargée avec succès !");
-  }
-
-  onImageError() {
-    console.error("Erreur lors du chargement de l'image !");
-  }
 }
